49ac80403bead16a7de4875a6f6dd7e1e499a446
Astropeak <astropeak@gmail.com>
2018-03-25 15:25:30 +0800

    Add install-emacs.sh

---
+++ install-emacs.sh
@@ -0,0 +1,7 @@
+mkdir ~/software
+cd ~/software
+git clone --recursive-submodules https://github.com/astropeak/emacs.d.git
+git clone --recursive-submodules https://github.com/astropeak/myelpa.git
+
+
+
