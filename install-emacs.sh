8da6e36b3ee146386eabcd2bd6366726f5da91dc
Astropeak <astropeak@gmail.com>
2018-03-25 17:07:37 +0800

    Create .emacs file in bash

--- install-emacs.sh
+++ install-emacs.sh
@@ -1,7 +1,34 @@
-mkdir ~/software
-cd ~/software
+if [ -z "$1" ]; then
+    echo "Please call '$0 INSTALL_DIR' to run this command!\n"
+    exit 1
+fi
+
+install_dir=$1
+
+if [ ! -d $install_dir ]; then
+    mkdir -p $install_dir
+    echo "Making directory: ${install_dir}"
+fi
+
+cd $install_dir
 git clone --recurse-submodules https://github.com/astropeak/emacs.d
 git clone https://github.com/astropeak/myelpa
 git clone https://github.com/astropeak/aspk-code-base
+
+dotemacs=~/.emacs
+if [ -f $dotemacs ]; then
+    DATE=`date '+%Y%m%d_%H%M%S'`
+    new_name="${dotemacs}_$DATE"
+    mv $dotemacs $new_name
+    echo "${dotemacs} exists, move it $new_name"
+fi
+
+# create the .emacs file
+cat >$dotemacs <<EOL
+(setq *no-window* t)
+(package-initialize)
+(load-file "${install_dir}/emacs.d/init.el")
+EOL
+
 
 
