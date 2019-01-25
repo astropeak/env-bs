642f43c8f694e4c0cfecf0960ffc4b296daaf40c
Astropeak <astropeak@gmail.com>
2019-01-25 16:41:46 +0800

    Make script works when called by sudo

--- base.sh
+++ base.sh
@@ -5,7 +5,12 @@ abspath (){
 root_dir=
 build_dir=
 install_dir=
-root_dir=~/software
+# user=`ps -o user= -p $$ | awk '{print $1}'`
+# user=`id -u -n`
+# current user name, without regarding sudo. All above not work
+# ref: https://stackoverflow.com/questions/3522341/identify-user-in-a-bash-script-called-by-sudo
+user=`who am i | awk '{print $1}'`
+root_dir=/home/${user}/software
 if [ ! -z "$1" ]; then
     root_dir=$1
     # echo "Please call '$0 ROOT_DIR' to run this command!\n"
--- install-pip.sh
+++ install-pip.sh
@@ -1,3 +1,7 @@
+# in this script, build_dir and install_dir will be set
+. "$(dirname $0)"/base.sh
+
+cd $build_dir
 # https://pip.pypa.io/en/stable/installing/
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
